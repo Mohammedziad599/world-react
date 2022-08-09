2ec0e67057dcf8efe21e025b792ee51e99a6f9d0
Mohammed Ziad <Mohammedziad599@gmail.com>
2022-08-09 13:23:57 +0300

    adding routes in the index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App/>}>
+          <Route path="" element={<Home />}/>
+          <Route path="country/:countryCode" element={<Details />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
