9f18c268b07fdad781a02c01711378e1fc758774
Mohammed Ziad <Mohammedziad599@gmail.com>
2022-08-18 11:42:47 +0300

    created a constants file

---
+++ src/utilities/Constants.js
@@ -0,0 +1,13 @@
+export const filterCodes = {
+  NO_FILTER: "",
+  AFRICA: "AFRICA",
+  AMERICAS: "AMERICAS",
+  ASIA: "ASIA",
+  EUROPE: "EUROPE",
+  OCEANIA: "OCEANIA",
+  FAVOURITES: "FAVOURITES"
+};
+
+export const dragTypes = {
+  COUNTRY_CARD: "CARD"
+};
