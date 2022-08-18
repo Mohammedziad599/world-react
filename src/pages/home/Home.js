c6094417a895901ccfae03bf2f36025b9edc0bc5
Mohammed Ziad <Mohammedziad599@gmail.com>
2022-08-18 12:05:44 +0300

    added the fetch to the home and to filter the data as well as searching functionality

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,217 +1,233 @@
 import "./Home.css";
 import Search from "../../components/search/Search";
-import {Grid, Paper} from "@mui/material";
+import {Alert, Grid, Paper, Snackbar} from "@mui/material";
 import FilterMenu from "../../components/filter-menu/FilterMenu";
 import Favourite from "../../components/favourite/Favourite";
 import CountriesList from "../../components/countries-list/CountriesList";
+import {useEffect, useState} from "react";
+import {getAllCountries, searchCountriesByName} from "../../utilities/requests/CountriesRequests";
+import {filterCodes} from "../../utilities/Constants";
+import {DndProvider} from "react-dnd";
+import {HTML5Backend} from "react-dnd-html5-backend";
+import {readFavouriteCountriesLocalstorage, setFavouriteCountriesLocalstorage} from "../../utilities/localstorage";
 
 function Home() {
-  return (<>
-    <Grid
-      container
-      sx={{
-        py: 2, justifyContent: "space-between"
-      }}
-    >
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [snackbarStatus, setSnackbarStatus] = useState(false);
+  const [snackbarType, setSnackbarType] = useState("success");
+  const [countries, setCountries] = useState([]);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+  const [favouriteCountries, setFavouriteCountries] = useState({});
+
+  function onSearchChange(value) {
+    if (value.trim() !== search.trim()) {
+      setSearch(value);
+      setLoading(true);
+    }
+  }
+
+  function closeSnackbar() {
+    setSnackbarStatus(false);
+  }
+
+  function onFavouriteDrop(item) {
+    setFavouriteCountries((prevState) => {
+      prevState[item.country.cca3] = {
+        name: item.country.name.common,
+        flag: item.country.flag
+      };
+      setFavouriteCountriesLocalstorage(prevState);
+      return prevState;
+    });
+  }
+
+  function onFavouriteCountryDelete(code) {
+    setFavouriteCountries((prevState) => {
+      delete prevState[code];
+
+      //TODO TEMP Solution
+      setFavouriteCountriesLocalstorage(prevState);
+      return prevState;
+    });
+  }
+
+  function setData(loading, countries, snackbarStatus, snackbarType, snackbarMessage) {
+    setLoading(loading);
+    setCountries(countries);
+    setSnackbarStatus(snackbarStatus);
+    setSnackbarType(snackbarType);
+    setSnackbarMessage(snackbarMessage);
+  }
+
+  function filterData(data, filter) {
+    let filteredData;
+    if (filter === filterCodes.NO_FILTER) {
+      filteredData = data;
+    } else if (filter === filterCodes.FAVOURITES) {
+      filteredData = data.filter();
+    } else {
+      filteredData = data.filter((country) => {
+        return country.region.toUpperCase() === filter;
+      });
+    }
+    return filteredData;
+  }
+
+  function onFilterChange(value) {
+    setFilter(value);
+  }
+
+  useEffect(() => {
+    setFavouriteCountries(readFavouriteCountriesLocalstorage());
+  }, []);
+
+  useEffect(() => {
+    if (search.trim() === "") {
+      getAllCountries().then(data => {
+        data = filterData(data, filter);
+        setData(false, data, false, "", "");
+      }).catch(error => {
+        setData(false, [], true, "error", error.message);
+      });
+    } else {
+      searchCountriesByName(search).then(data => {
+        setData(false, data, false, "", "");
+      }).catch(() => {
+        setData(false, [], true, "error", "No countries found");
+      });
+    }
+  }, [search, filter]);
+
+
+  //TODO fix this
+  // useEffect(() => {
+  //   console.log("useEffect");
+  //   console.log(favouriteCountries);
+  // }, [favouriteCountries]);
+
+  return (
+    <>
       <Grid
-        xs={12}
-        md={5}
-        lg={4}
-        item={true}
+        container
         sx={{
-          my: 1.5
+          py: 2, justifyContent: "space-between"
         }}
       >
-        <Search
-          placeholder="Search"
-        />
-      </Grid>
-      <Grid
-        xs={6}
-        md={2}
-        lg={2}
-        item={true}
-        sx={{
-          my: 1.5
-        }}
-      >
-        <Paper
-          elevation={1}
+        <Grid
+          item
+          display="flex"
+          xs={12}
+          md={5}
+          lg={4}
           sx={{
-            width: "100%",
+            my: 1.5
           }}
         >
-          <FilterMenu
+          <Search
             sx={{
-              width: "100%",
-              py: 1.75,
+              flexGrow: 1
+            }}
+            placeholder="Search"
+            onEnter={onSearchChange}
+          />
+        </Grid>
+        <Grid
+          item
+          xs={6}
+          md={2}
+          lg={2}
+          sx={{
+            my: 1.5
+          }}
+        >
+          <Paper
+            elevation={1}
+            sx={{
+              width: "100%"
             }}
           >
-          </FilterMenu>
-        </Paper>
-      </Grid>
-    </Grid>
-    <Grid
-      container
-      sx={{
-        height: {
-          xs: "fit-content",
-          lg: "calc(100vh - 10.85rem)"
-        },
-      }}
-    >
-      <Grid
-        item
-        lg={3}
-        sx={{
-          pr: 2,
-          display: {
-            xs: "none",
-            lg: "block"
-          }
-        }}>
-        <Favourite
-          sx={{
-            height: {
-              xs: "0",
-              lg: "calc(100vh - 11.5rem)"
-            },
-            overflow: "scroll"
-          }}
-          countries={[{
-            name: "Palestine", code: "PSE"
-          }]}
-        />
+            <FilterMenu
+              sx={{
+                width: "100%", py: 1.75
+              }}
+              onFilterChange={onFilterChange}
+            >
+            </FilterMenu>
+          </Paper>
+        </Grid>
       </Grid>
       <Grid
+        container
         sx={{
           height: {
-            xs: "0",
-            lg: "calc(100vh - 11rem)"
-          },
-          overflow: "scroll"
+            xs: "fit-content", lg: "calc(100vh - 11rem)"
+          }
         }}
-        item
-        xs={12}
-        lg={9}>
-        <CountriesList countries={[{
-          "flags": {
-            "png": "https://flagcdn.com/w320/kw.png", "svg": "https://flagcdn.com/kw.svg"
-          }, "name": {
-            "common": "Kuwait", "official": "State of Kuwait", "nativeName": {
-              "ara": {
-                "official": "دولة الكويت", "common": "الكويت"
+      >
+        <DndProvider backend={HTML5Backend}>
+          <Grid
+            item
+            lg={3}
+            sx={{
+              pr: 2, display: {
+                xs: "none", lg: "block"
               }
+            }}>
+            <Favourite
+              sx={{
+                height: {
+                  xs: "0", lg: "calc(100vh - 11.8rem)"
+                },
+                overflow: "scroll"
+              }}
+              onDrop={onFavouriteDrop}
+              onCountryDelete={onFavouriteCountryDelete}
+              countries={favouriteCountries}
+            />
+          </Grid>
+          <Grid
+            sx={{
+              height: {
+                xs: "0", lg: "calc(100vh - 11rem)"
+              },
+              flexGrow: "1 !important",
+              maxWidth: "100% !important",
+              overflowY: "scroll",
+              margin: "0px -0.5rem",
+              padding: "0px 0.5rem"
+            }}
+            item
+            xs={12}
+            lg={9}>
+            {
+              (countries.length !== 0) && (
+                <CountriesList
+                  countries={countries}
+                  loading={loading}/>
+              )
             }
-          }, "code": "KWT", "capital": ["Kuwait City"], "region": "Asia", "population": 4270563
-        }, {
-          "flags": {
-            "png": "https://flagcdn.com/w320/pw.png", "svg": "https://flagcdn.com/pw.svg"
-          }, "name": {
-            "common": "Palau", "official": "Republic of Palau", "nativeName": {
-              "eng": {
-                "official": "Republic of Palau", "common": "Palau"
-              }, "pau": {
-                "official": "Beluu er a Belau", "common": "Belau"
-              }
-            }
-          }, "code": "PLW", "capital": ["Ngerulmud"], "region": "Oceania", "population": 18092
-        }, {
-          "flags": {
-            "png": "https://flagcdn.com/w320/li.png", "svg": "https://flagcdn.com/li.svg"
-          }, "name": {
-            "common": "Liechtenstein", "official": "Principality of Liechtenstein", "nativeName": {
-              "deu": {
-                "official": "Fürstentum Liechtenstein", "common": "Liechtenstein"
-              }
-            }
-          }, "code": "LIE", "capital": ["Vaduz"], "region": "Europe", "population": 38137
-        }, {
-          "flags": {
-            "png": "https://flagcdn.com/w320/md.png", "svg": "https://flagcdn.com/md.svg"
-          }, "name": {
-            "common": "Moldova", "official": "Republic of Moldova", "nativeName": {
-              "ron": {
-                "official": "Republica Moldova", "common": "Moldova"
-              }
-            }
-          }, "code": "MDA", "capital": ["Chișinău"], "region": "Europe", "population": 2617820
-        }, {
-          "flags": {
-            "png": "https://flagcdn.com/w320/ao.png", "svg": "https://flagcdn.com/ao.svg"
-          }, "name": {
-            "common": "Angola", "official": "Republic of Angola", "nativeName": {
-              "por": {
-                "official": "República de Angola", "common": "Angola"
-              }
-            }
-          }, "code": "AGO", "capital": ["Luanda"], "region": "Africa", "population": 32866268
-        }, {
-          "flags": {
-            "png": "https://flagcdn.com/w320/bo.png", "svg": "https://flagcdn.com/bo.svg"
-          }, "name": {
-            "common": "Bolivia", "official": "Plurinational State of Bolivia", "nativeName": {
-              "aym": {
-                "official": "Wuliwya Suyu", "common": "Wuliwya"
-              }, "grn": {
-                "official": "Tetã Volívia", "common": "Volívia"
-              }, "que": {
-                "official": "Buliwya Mamallaqta", "common": "Buliwya"
-              }, "spa": {
-                "official": "Estado Plurinacional de Bolivia", "common": "Bolivia"
-              }
-            }
-          }, "code": "BOL", "capital": ["Sucre"], "region": "Americas", "population": 11673029
-        }, {
-          "flags": {
-            "png": "https://flagcdn.com/w320/tv.png", "svg": "https://flagcdn.com/tv.svg"
-          }, "name": {
-            "common": "Tuvalu", "official": "Tuvalu", "nativeName": {
-              "eng": {
-                "official": "Tuvalu", "common": "Tuvalu"
-              }, "tvl": {
-                "official": "Tuvalu", "common": "Tuvalu"
-              }
-            }
-          }, "code": "TUV", "capital": ["Funafuti"], "region": "Oceania", "population": 11792
-        }, {
-          "flags": {
-            "png": "https://flagcdn.com/w320/pe.png", "svg": "https://flagcdn.com/pe.svg"
-          }, "name": {
-            "common": "Peru", "official": "Republic of Peru", "nativeName": {
-              "aym": {
-                "official": "Piruw Suyu", "common": "Piruw"
-              }, "que": {
-                "official": "Piruw Ripuwlika", "common": "Piruw"
-              }, "spa": {
-                "official": "República del Perú", "common": "Perú"
-              }
-            }
-          }, "code": "PER", "capital": ["Lima"], "region": "Americas", "population": 32971846
-        }, {
-          "flags": {
-            "png": "https://flagcdn.com/w320/nc.png", "svg": "https://flagcdn.com/nc.svg"
-          }, "name": {
-            "common": "New Caledonia", "official": "New Caledonia", "nativeName": {
-              "fra": {
-                "official": "Nouvelle-Calédonie", "common": "Nouvelle-Calédonie"
-              }
-            }
-          }, "code": "NCL", "capital": ["Nouméa"], "region": "Oceania", "population": 271960
-        }, {
-          "flags": {
-            "png": "https://flagcdn.com/w320/de.png", "svg": "https://flagcdn.com/de.svg"
-          }, "name": {
-            "common": "Germany", "official": "Federal Republic of Germany", "nativeName": {
-              "deu": {
-                "official": "Bundesrepublik Deutschland", "common": "Deutschland"
-              }
-            }
-          }, "code": "DEU", "capital": ["Berlin"], "region": "Europe", "population": 83240525
-        },]}/>
+          </Grid>
+        </DndProvider>
       </Grid>
-    </Grid>
-  </>);
+      {
+        (snackbarStatus) && (
+          <Snackbar
+            sx={{
+              boxShadow: "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)"
+            }}
+            open={snackbarStatus}
+            message={"Not Found"}
+          >
+            <Alert onClose={closeSnackbar} variant="filled" severity={snackbarType} sx={{width: "100%"}}>
+              {snackbarMessage}
+            </Alert>
+          </Snackbar>
+        )
+      }
+    </>
+  );
 }
 
 export default Home;
