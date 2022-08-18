0f23f780ec2d09100338d933bd552fb2255dcb25
Mohammed Ziad <Mohammedziad599@gmail.com>
2022-08-18 12:03:05 +0300

    updated the Dropdown

--- src/components/dropdown/Dropdown.js
+++ src/components/dropdown/Dropdown.js
@@ -1,58 +1,58 @@
-import './Dropdown.css';
+import "./Dropdown.css";
 import {Button, Menu, MenuItem, useTheme} from "@mui/material";
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import {useState} from "react";
-import {v4 as uuid} from "uuid";
 
-function Dropdown(props) {
-
-  const {
-    items = []
-  } = props;
-
+function Dropdown({
+                    items = [], sx = {}, iconLeft = "",
+                    label = "", iconRight = {}, enableDropdownArrow = false
+                  }) {
   const [menu, setMenu] = useState(null);
   const theme = useTheme();
 
   const itemsContent = items.map(item => {
-    return (<MenuItem
-        key={uuid()}
+    return (
+      <MenuItem
+        key={item.code}
         onClick={() => item.onClick(item) || handleClose()}
-        {...item.itemProp}
       >
         {item.iconLeft}
         {item.label}
         {item.iconRight}
-      </MenuItem>)
+      </MenuItem>
+    );
   });
 
   const handleClick = (event) => {
     setMenu(event.currentTarget);
-  }
+  };
 
   const handleClose = () => {
     setMenu(null);
-  }
+  };
 
-  return (<>
+  return (
+    <>
       <Button
         aria-controls={!!menu ? "menu" : undefined}
         aria-haspopup="true"
         aria-expanded={!!menu ? "true" : undefined}
         onClick={handleClick}
         sx={{
-          color: theme.palette.mode === "dark" ? theme.palette.grey["100"] : theme.palette.grey["900"], ":hover": {
+          color: theme.palette.mode === "dark" ? theme.palette.grey["100"] : theme.palette.grey["900"],
+          ":hover": {
             backgroundColor: theme.palette.mode === "dark" ? theme.palette.grey["800"] : theme.palette.grey["200"]
-          }, ...props.sx
+          },
+          ...sx
         }}
       >
-        {props.iconLeft}
-        {props.label}
-        {props.iconRight}
-        {props.enableDropdownArrow ? (!!menu ? <KeyboardArrowUpIcon sx={{ml: 0.5}}/> :
+        {iconLeft}
+        {label}
+        {iconRight}
+        {enableDropdownArrow ? (!!menu ? <KeyboardArrowUpIcon sx={{ml: 0.5}}/> :
           <KeyboardArrowDownIcon sx={{ml: 0.5}}/>) : ("")}
       </Button>
-      {/*TODO Fix menu Width not the same as the button width.*/}
       <Menu
         anchorEl={menu}
         open={!!menu}
@@ -60,7 +60,8 @@ function Dropdown(props) {
       >
         {itemsContent}
       </Menu>
-    </>);
+    </>
+  );
 }
 
 export default Dropdown;
