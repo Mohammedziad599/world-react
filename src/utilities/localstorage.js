331dca9830e0e9384ccf9a10d885e25a53f12a1f
Mohammed Ziad <Mohammedziad599@gmail.com>
2022-08-18 11:48:29 +0300

    created a localstorage file

---
+++ src/utilities/localstorage.js
@@ -0,0 +1,9 @@
+import {filterCodes} from "./Constants";
+
+export function setFavouriteCountriesLocalstorage(countries){
+  localStorage.setItem(filterCodes.FAVOURITES, JSON.stringify(countries));
+}
+
+export function readFavouriteCountriesLocalstorage(){
+  return JSON.parse(localStorage.getItem(filterCodes.FAVOURITES)) || [];
+}
