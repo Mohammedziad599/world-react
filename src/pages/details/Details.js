a4ad0cc1ba2a9358614ade9bdd3b40e10a9e5be5
Mohammed Ziad <Mohammedziad599@gmail.com>
2022-08-18 12:06:06 +0300

    implement the fetch functionality in the Details

--- src/pages/details/Details.js
+++ src/pages/details/Details.js
@@ -1,12 +1,44 @@
 import "./Details.css";
 import {Link, useParams} from "react-router-dom";
 import {Button, Grid, Paper, useTheme} from "@mui/material";
-import ArrowBackOutlinedIcon from '@mui/icons-material/ArrowBackOutlined';
+import ArrowBackOutlinedIcon from "@mui/icons-material/ArrowBackOutlined";
 import CountryDetails from "../../components/country-details/CountryDetails";
+import {useEffect, useState} from "react";
+import {getCountryDetailsFromCode, getCountryName} from "../../utilities/requests/CountriesRequests";
 
 function Details() {
   const {countryCode} = useParams();
+
+  const [loading, setLoading] = useState(true);
+  const [countryDetail, setCountryDetail] = useState({});
+
   const theme = useTheme();
+
+  useEffect(() => {
+    setLoading(true);
+    getCountryDetailsFromCode(countryCode)
+      .then(data => {
+        let fetches = [];
+        let borders = [];
+        for (const countryCode of data.borders) {
+          fetches.push(
+            getCountryName(countryCode)
+              .then(country => borders.push({
+                code: countryCode,
+                name: country.name.common
+              }))
+              .catch(error => console.error(error))
+          );
+        }
+        Promise.all(fetches).then(() => {
+          data.borders = borders;
+          setCountryDetail(data);
+          setLoading(false);
+        });
+      })
+      .catch(error => console.error(error));
+  }, [countryCode]);
+
   return (
     <>
       <Grid
@@ -27,7 +59,7 @@ function Details() {
               color: theme.palette.mode === "dark" ? theme.palette.grey["100"] : theme.palette.grey["900"],
               ":hover": {
                 backgroundColor: theme.palette.mode === "dark" ? theme.palette.grey["800"] : theme.palette.grey["200"]
-              },
+              }
             }}
           >
             <ArrowBackOutlinedIcon
@@ -39,8 +71,11 @@ function Details() {
           </Button>
         </Paper>
       </Grid>
-      <Grid>
-        <CountryDetails/>
+      <Grid container>
+        <CountryDetails
+          loading={loading}
+          country={countryDetail}
+        />
       </Grid>
     </>
   );
