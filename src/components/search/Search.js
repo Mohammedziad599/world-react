62113f929e4b4a408fbe5f02be4ca8b3fd5a838c
Mohammed Ziad <Mohammedziad599@gmail.com>
2022-08-18 11:49:37 +0300

    added onEnter callback to the search component

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,12 +1,18 @@
-import './Search.css';
-import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import "./Search.css";
+import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 import {Box, Input, Paper, useTheme} from "@mui/material";
 import {useState} from "react";
 
-function Search(props) {
+function Search({onEnter, sx = {}}) {
   const theme = useTheme();
 
   const [searchElement, setSearchElement] = useState(null);
+
+  function onKeyDown(event) {
+    if (event.nativeEvent.keyCode === 13) {
+      onEnter(event.target.value);
+    }
+  }
 
   return (
     <Paper
@@ -18,14 +24,15 @@ function Search(props) {
         border: "2px solid transparent",
         ":focus-within": {
           border: "2px solid " + theme.palette.primary.main
-        }
+        },
+        ...sx
       }}
     >
       <Box
         onClick={() => searchElement.focus()}
-        sx={{display: 'flex', alignItems: 'center', paddingX: 1.5, paddingY: 1}}>
+        sx={{display: "flex", alignItems: "center", paddingX: 1.5, paddingY: 1}}>
         <SearchRoundedIcon
-          sx={{color: 'action.active', mr: 1, my: 0.5, ml: 2}}
+          sx={{color: "action.active", mr: 1, my: 0.5, ml: 2}}
         />
         <Input
           id="search-input"
@@ -35,12 +42,15 @@ function Search(props) {
           }}
           sx={{
             width: "100%",
+            mr: 1
           }}
           type="text"
+          onKeyDown={onKeyDown}
           disableUnderline={true}
         />
       </Box>
-    </Paper>);
+    </Paper>
+  );
 }
 
 export default Search;
