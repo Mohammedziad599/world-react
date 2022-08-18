c8b0c57e24305693cd368f2077c0b085a57f1227
Mohammed Ziad <Mohammedziad599@gmail.com>
2022-08-18 11:58:20 +0300

    implemented the ThemeSwitcher

--- src/components/theme-switcher/ThemeSwitcher.js
+++ src/components/theme-switcher/ThemeSwitcher.js
@@ -1,48 +1,51 @@
-import './ThemeSwitcher.css';
+import "./ThemeSwitcher.css";
 import Dropdown from "../dropdown/Dropdown";
-import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
-import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
-import AutoAwesomeOutlinedIcon from '@mui/icons-material/AutoAwesomeOutlined';
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import AutoAwesomeOutlinedIcon from "@mui/icons-material/AutoAwesomeOutlined";
+import {useContext} from "react";
+import ThemeContext from "../../contexts/ThemeContext/ThemeContext";
+import {themeCodes} from "../../utilities/Constants";
 
-function ThemeSwitcher(props) {
-  const setThemeLight = (event) => {
-    //TODO
-    console.log("Light");
-  }
-  const setThemeDark = (event) => {
-    //TODO
-    console.log("Dark");
-  }
-  const setThemeAuto = (event) => {
-    //TODO
-    console.log("System");
-  }
+function ThemeSwitcher({sx = {}}) {
+  const [currentTheme, setCurrentTheme] = useContext(ThemeContext);
 
+  const setTheme = (item) => {
+    setCurrentTheme(item.code);
+  };
+
+  const leftIcon = (currentTheme === themeCodes.AUTO ?
+    <AutoAwesomeOutlinedIcon sx={{mr: 0.5}}/> :
+    (currentTheme === themeCodes.DARK ? <DarkModeOutlinedIcon sx={{mr: 0.5}}/> :
+      <LightModeOutlinedIcon sx={{mr: 0.5}}/>));
+
+  const items = [
+    {
+      label: "Light",
+      iconLeft: <LightModeOutlinedIcon sx={{mr: 0.5}}/>,
+      code: themeCodes.LIGHT,
+      onClick: setTheme
+    },
+    {
+      label: "Dark",
+      iconLeft: <DarkModeOutlinedIcon sx={{mr: 0.5}}/>,
+      code: themeCodes.DARK,
+      onClick: setTheme
+    },
+    {
+      label: "Auto",
+      iconLeft: <AutoAwesomeOutlinedIcon sx={{mr: 0.5}}/>,
+      code: themeCodes.AUTO,
+      onClick: setTheme
+    }
+  ];
 
   return (
     <Dropdown
-      sx={{...props.sx}}
-      label={"Theme"}
-      iconLeft={<LightModeOutlinedIcon sx={{mr: 0.5}}/>}
-      items={
-        [
-          {
-            label: "Light",
-            iconLeft: <LightModeOutlinedIcon sx={{mr: 0.5}}/>,
-            click: setThemeLight
-          },
-          {
-            label: "Dark",
-            iconLeft: <DarkModeOutlinedIcon sx={{mr: 0.5}}/>,
-            click: setThemeDark
-          },
-          {
-            label: "System",
-            iconLeft: <AutoAwesomeOutlinedIcon sx={{mr: 0.5}}/>,
-            click: setThemeAuto
-          },
-        ]
-      }
+      sx={{...sx}}
+      label={currentTheme === themeCodes.AUTO ? "Auto" : (currentTheme === themeCodes.DARK ? "Dark" : "Light")}
+      iconLeft={leftIcon}
+      items={items}
     />
   );
 }
