6063f672b08d1f0d6c053da63075b2689729f937
Mohammed Ziad <Mohammedziad599@gmail.com>
2022-08-18 11:47:31 +0300

    added the loading state to CountryDetails and moved some code to CountryInformation

--- src/components/country-details/CountryDetails.js
+++ src/components/country-details/CountryDetails.js
@@ -1,24 +1,39 @@
-import './CountryDetails.css';
-import {Button, Grid, Paper, Stack, Typography, useTheme} from "@mui/material";
-import {Link} from "react-router-dom";
+import "./CountryDetails.css";
+import {Grid, Skeleton} from "@mui/material";
+import CountryInformation from "../country-information/CountryInformation";
 
-function CountryDetails(props) {
-  const theme = useTheme();
+function CountryDetails({country = {}, loading = false}) {
+  const {
+    name = {},
+    flags = {}
+  } = country;
 
-  return (
-    <>
-      <Grid
-        container
-        columns={{
-          xs: 1,
-          lg: 2,
-        }}
-        spacing={4}
-      >
-        <Grid item xs={1} md={1}>
+  const flagElement = loading ? (
+        <Grid
+          item
+          xs={1}
+          md={1}
+        >
+          <Skeleton
+            sx={{
+              height: {
+                xs: "15rem", sm: "25rem", md: "35rem", lg: "20rem", xl: "20rem"
+              }
+            }}
+            animation="wave"
+            variant="rounded"
+          />
+        </Grid>
+      ) :
+      (
+        <Grid
+          item
+          xs={1}
+          md={1}
+        >
           <img
-            src="https://flagcdn.com/kw.svg"
-            alt="Kuwait Flag"
+            src={flags.svg}
+            alt={`${name.common} Flag`}
             style={{
               width: "100%",
               filter: "drop-shadow(0 10px 8px rgb(0 0 0 / 0.04)) drop-shadow(0 4px 3px rgb(0 0 0 / 0.1))",
@@ -26,230 +41,26 @@ function CountryDetails(props) {
             }}
           />
         </Grid>
-        <Grid
-          item xs={1}
-          md={1}
-        >
-          <Typography
-            variant="h3"
-            sx={{
-              mb: 3
-            }}
-          >
-            Kuwait
-          </Typography>
-          <Grid
-            display="flex"
-            direction={{
-              xs: "column",
-              md: "row"
-            }}
-            sx={{
-              justifyContent: "space-between"
-            }}
-          >
-            <Stack
-              item
-              sx={{
-                my: 2
-              }}
-            >
-              <Typography
-                sx={{
-                  my: 0.25
-                }}
-                variant="p">
-                <Typography
-                  component="span"
-                  sx={{
-                    fontWeight: "bold",
-                    mr: 0.5
-                  }}>
-                  Native Name:
-                </Typography>
-                kuwait
-              </Typography>
-              <Typography
-                sx={{
-                  my: 0.25
-                }}
-                variant="p">
-                <Typography
-                  component="span"
-                  sx={{
-                    fontWeight: "bold",
-                    mr: 0.5
-                  }}>
-                  Population:
-                </Typography>
-                4,270,563
-              </Typography>
-              <Typography
-                sx={{
-                  my: 0.25
-                }}
-                variant="p">
-                <Typography
-                  component="span"
-                  sx={{
-                    fontWeight: "bold",
-                    mr: 0.5
-                  }}>
-                  Region:
-                </Typography>
-                Asia
-              </Typography>
-              <Typography
-                sx={{
-                  my: 0.25
-                }}
-                variant="p">
-                <Typography
-                  component="span"
-                  sx={{
-                    fontWeight: "bold",
-                    mr: 0.5
-                  }}>
-                  Sub Region:
-                </Typography>
-                Western Asia
-              </Typography>
-              <Typography
-                sx={{
-                  my: 0.25
-                }}
-                variant="p">
-                <Typography
-                  component="span"
-                  sx={{
-                    fontWeight: "bold",
-                    mr: 0.5
-                  }}>
-                  Capital:
-                </Typography>
-                Kuwait City
-              </Typography>
-            </Stack>
-            <Stack
-              item
-              sx={{
-                my: 2
-              }}
-            >
-              <Typography
-                sx={{
-                  my: 0.25
-                }}
-                variant="p">
-                <Typography
-                  component="span"
-                  sx={{
-                    fontWeight: "bold",
-                    mr: 0.5
-                  }}>
-                  Top Level Domain:
-                </Typography>
-                .kw
-              </Typography>
-              <Typography
-                sx={{
-                  my: 0.25
-                }}
-                variant="p">
-                <Typography
-                  component="span"
-                  sx={{
-                    fontWeight: "bold",
-                    mr: 0.5
-                  }}>
-                  Currencies:
-                </Typography>
-                Kuwaiti dinar
-              </Typography>
-              <Typography
-                sx={{
-                  my: 0.25
-                }}
-                variant="p">
-                <Typography
-                  component="span"
-                  sx={{
-                    fontWeight: "bold",
-                    mr: 0.5
-                  }}>
-                  Languages:
-                </Typography>
-                Arabic
-              </Typography>
-            </Stack>
-          </Grid>
-          <Grid
-            display="flex"
-            direction="row"
-            sx={{
-              py: 6,
-              flexWrap: "wrap"
-            }}
-            flexWrap>
-            <Typography
-              variant="p"
-              sx={{
-                width: {
-                  xs: "100%",
-                  md: "fit-content"
-                },
-                fontWeight: "bold",
-                mr: 1,
-                display: "flex",
-                alignItems: "center"
-              }}>
-              Border Countries:
-            </Typography>
-              {/*TODO put this in the function body with an array map*/}
-              <Paper
-                sx={{
-                  mr: 1.5,
-                  my: 1
-                }}
-                elevation={1}>
-                <Button
-                  component={Link}
-                  to={"/"}
-                  sx={{
-                    py: 1,
-                    px: 1.5,
-                    color: theme.palette.mode === "dark" ? theme.palette.grey["100"] : theme.palette.grey["900"],
-                    ":hover": {
-                      backgroundColor: theme.palette.mode === "dark" ? theme.palette.grey["800"] : theme.palette.grey["200"]
-                    },
-                  }}
-                >
-                  Iraq
-                </Button>
-              </Paper>
-              <Paper
-                sx={{
-                  mr: 1.5,
-                  my: 1
-                }}
-                elevation={1}>
-                <Button
-                  component={Link}
-                  to={"/"}
-                  sx={{
-                    py: 1,
-                    px: 1.5,
-                    color: theme.palette.mode === "dark" ? theme.palette.grey["100"] : theme.palette.grey["900"],
-                    ":hover": {
-                      backgroundColor: theme.palette.mode === "dark" ? theme.palette.grey["800"] : theme.palette.grey["200"]
-                    },
-                  }}
-                >
-                  Saudi Arabia
-                </Button>
-              </Paper>
-          </Grid>
-        </Grid>
+      )
+  ;
+
+  if (!country.name) {
+    return null;
+  }
+
+  return (
+    <>
+      <Grid
+        container
+        columns={{
+          xs: 1, lg: 2
+        }}
+        spacing={4}
+      >
+        {flagElement}
+        <CountryInformation
+          loading={loading}
+          country={country}/>
       </Grid>
     </>
   );
---
+++ src/components/country-information/CountryInformation.js
@@ -0,0 +1,294 @@
+import "./CountryInformation.css";
+import {Button, Grid, Paper, Skeleton, Stack, Typography, useTheme} from "@mui/material";
+import {Link} from "react-router-dom";
+
+function CountryInformation({country = {}, loading = false}) {
+  const {
+    name = {},
+    nativeName = name.nativeName[Object.keys(name.nativeName)[0]].common,
+    population = 0,
+    region = "",
+    subregion = "",
+    capital = [],
+    tld = [],
+    currencies = {},
+    currenciesKeys = Object.keys(currencies).map(key => {
+      return key;
+    }),
+    languages = {},
+    languagesNames = Object.keys(languages).map(key => {
+      return languages[key];
+    }),
+    borders = []
+  } = country;
+
+  const theme = useTheme();
+
+  const leftListElements = loading ? (
+    <Stack
+      sx={{
+        my: 2,
+        width: "100%"
+      }}
+    >
+      <Skeleton
+        height={"1rem"}
+        sx={{
+          mb: 1.5,
+          width: "100%"
+        }}
+        animation="wave" variant="rounded"/>
+      <Skeleton
+        height={"1rem"}
+        sx={{
+          mb: 1.5,
+          width: "100%"
+        }}
+        animation="wave" variant="rounded"/>
+      <Skeleton
+        height={"1rem"}
+        sx={{
+          mb: 1.5,
+          width: "100%"
+        }}
+        animation="wave" variant="rounded"/>
+      <Skeleton
+        height={"1rem"}
+        sx={{
+          mb: 1.5,
+          width: "100%"
+        }}
+        animation="wave" variant="rounded"/>
+      <Skeleton
+        height={"1rem"}
+        sx={{
+          mb: 1.5,
+          width: "100%"
+        }}
+        animation="wave" variant="rounded"/>
+    </Stack>
+  ) : (
+    <Stack
+      sx={{
+        my: 2
+      }}
+    >
+      <Typography
+        sx={{
+          my: 0.25
+        }}
+        variant="p">
+        <Typography
+          component="span"
+          sx={{
+            fontWeight: "bold", mr: 0.5
+          }}>
+          Native Name:
+        </Typography>
+        {nativeName}
+      </Typography>
+      <Typography
+        sx={{
+          my: 0.25
+        }}
+        variant="p">
+        <Typography
+          component="span"
+          sx={{
+            fontWeight: "bold", mr: 0.5
+          }}>
+          Population:
+        </Typography>
+        {population}
+      </Typography>
+      <Typography
+        sx={{
+          my: 0.25
+        }}
+        variant="p">
+        <Typography
+          component="span"
+          sx={{
+            fontWeight: "bold", mr: 0.5
+          }}>
+          Region:
+        </Typography>
+        {region}
+      </Typography>
+      <Typography
+        sx={{
+          my: 0.25
+        }}
+        variant="p">
+        <Typography
+          component="span"
+          sx={{
+            fontWeight: "bold", mr: 0.5
+          }}>
+          Sub Region:
+        </Typography>
+        {subregion}
+      </Typography>
+      <Typography
+        sx={{
+          my: 0.25
+        }}
+        variant="p">
+        <Typography
+          component="span"
+          sx={{
+            fontWeight: "bold", mr: 0.5
+          }}>
+          Capital:
+        </Typography>
+        {capital}
+      </Typography>
+    </Stack>
+  );
+
+  const rightListElements = loading ? ("") : (
+    <Stack
+      sx={{
+        my: 2
+      }}
+    >
+      <Typography
+        sx={{
+          my: 0.25
+        }}
+        variant="p">
+        <Typography
+          component="span"
+          sx={{
+            fontWeight: "bold", mr: 0.5
+          }}>
+          Top Level Domain:
+        </Typography>
+        {tld.join(" ,")}
+      </Typography>
+      <Typography
+        sx={{
+          my: 0.25
+        }}
+        variant="p">
+        <Typography
+          component="span"
+          sx={{
+            fontWeight: "bold", mr: 0.5
+          }}>
+          Currencies:
+        </Typography>
+        {currenciesKeys.join(" ,")}
+      </Typography>
+      <Typography
+        sx={{
+          my: 0.25
+        }}
+        variant="p">
+        <Typography
+          component="span"
+          sx={{
+            fontWeight: "bold", mr: 0.5
+          }}>
+          Languages:
+        </Typography>
+        {languagesNames.join(" ,")}
+      </Typography>
+    </Stack>
+  );
+
+  const borderCountries = borders.map(border => {
+    return (
+      <Paper
+        key={border.code}
+        sx={{
+          mr: 1.5, my: 1
+        }}
+        elevation={1}>
+        <Button
+          component={Link}
+          to={`/country/${border.code}`}
+          sx={{
+            py: 1,
+            px: 1.5,
+            color: theme.palette.mode === "dark" ? theme.palette.grey["100"] : theme.palette.grey["900"],
+            ":hover": {
+              backgroundColor: theme.palette.mode === "dark" ? theme.palette.grey["800"] : theme.palette.grey["200"]
+            }
+          }}
+        >
+          {border.name}
+        </Button>
+      </Paper>
+    );
+  });
+
+  let titleElement = loading ? (
+    <Skeleton
+      height={"2rem"}
+      sx={{
+        mb: 3,
+        width: "50%"
+      }}
+      animation="wave" variant="rounded"/>
+  ) : (
+    <Typography
+      variant="h4"
+      sx={{
+        mb: 3
+      }}>
+      {name.common}
+    </Typography>
+  );
+
+  return (
+    <Grid
+      item
+      xs={1}
+      md={1}>
+      {titleElement}
+      <Grid
+        container
+        display="flex"
+        direction={{
+          xs: "column", md: "row"
+        }}
+        sx={{
+          justifyContent: "space-between"
+        }}>
+        {leftListElements}
+        {rightListElements}
+      </Grid>
+      <Grid
+        display="flex"
+        sx={{
+          py: 6, flexWrap: "wrap", flexDirection: "row"
+        }}
+        flexWrap
+      >
+        {loading ? (
+          <Skeleton
+            height={"1.5rem"}
+            sx={{
+              width: "100%"
+            }}
+            animation="wave"
+            variant="rounded"/>
+        ) : (
+          <Typography
+            variant="p"
+            sx={{
+              width: {
+                xs: "100%", md: "fit-content"
+              }, fontWeight: "bold", mr: 1, display: "flex", alignItems: "center"
+            }}>
+            Border Countries:
+          </Typography>
+        )}
+        {loading ? "" : borderCountries}
+      </Grid>
+    </Grid>
+  );
+}
+
+export default CountryInformation;
