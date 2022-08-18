894a7a768ebacfdc4870046acd00eda17b9e2b94
Mohammed Ziad <Mohammedziad599@gmail.com>
2022-08-18 11:46:05 +0300

    added the loading state to CountryList

--- src/components/countries-list/CountriesList.js
+++ src/components/countries-list/CountriesList.js
@@ -1,41 +1,61 @@
-import './CountriesList.css';
+import "./CountriesList.css";
 import CountryCard from "../country-card/CountryCard";
 import {Box} from "@mui/material";
 
+function CountriesList({countries = [], loading = false, sx = {}}) {
+  let countriesCards;
 
-//TODO decide if this container is a smart component or a dump component
-function CountriesList(props) {
-  const {
-    countries = []
-  } = props;
+  if (loading) {
+    countriesCards = new Array(9).fill(0).map((value, index) => {
+      return (
+        <Box
+          key={index}
+          sx={{
+            mb: 4
+          }}
+        >
+          <CountryCard
+            country={{}}
+            loading={loading}
+          />
+        </Box>
+      );
+    });
+  } else {
+    countriesCards = countries.map((country) => {
+      return (
+        <Box
+          sx={{
+            display: "flex",
+            mb: 4
+          }}
+          key={country.cca3}>
+          <CountryCard
+            sx={{
+              flexGrow: 1,
+              height: "100%"
 
-  const countriesCards = countries.map((country) => {
-    return (
-      <Box key={country.code}>
-        <CountryCard
-          country={country}
-        />
-      </Box>
-    );
-  });
+            }}
+            country={country}
+          />
+        </Box>
+      );
+    });
+  }
 
   return (
-    <>
-      <Box
-        display="grid"
-        sx={{
-          gridTemplateColumns: {
-            xs: "repeat(1, minmax(0, 1fr));",
-            md: "repeat(2, minmax(0, 1fr));",
-            xl: "repeat(3, minmax(0, 1fr));"
-          },
-          columnGap: "2rem",
-          rowGap: "2rem",
-        }}
-      >
-        {countriesCards}
-      </Box>
-    </>
+    <Box
+      display="grid"
+      sx={{
+        gridTemplateColumns: {
+          xs: "repeat(1, minmax(0, 1fr));", md: "repeat(2, minmax(0, 1fr));", xl: "repeat(3, minmax(0, 1fr));"
+        },
+        columnGap: "2rem",
+        ...sx
+      }}
+    >
+      {countriesCards}
+    </Box>
   );
 }
 
